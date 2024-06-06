6f47ebcc20646b69285f65d36c1210e5d1cb7197
DimaGalchenko <galchenkaol@gmail.com>
2024-06-06 14:30:14 +0300

    * implement winner modal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,5 +1,10 @@
 import showModal from './modal';
+import { createFighterImage } from '../fighterPreview';
 
 export default function showWinnerModal(fighter) {
     // call showModal function
+    showModal({
+        title: `Winner is ${fighter.name}`,
+        bodyElement: createFighterImage(fighter)
+    });
 }
