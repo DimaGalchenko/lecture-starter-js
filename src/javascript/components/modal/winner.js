6b4ae1a002fb95c840ba6843edd410d727db5f7d
DimaGalchenko <galchenkaol@gmail.com>
2024-06-06 15:11:31 +0300

    * add fighter name to modal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,10 +1,22 @@
 import showModal from './modal';
 import { createFighterImage } from '../fighterPreview';
+import createElement from '../../helpers/domHelper';
 
 export default function showWinnerModal(fighter) {
     // call showModal function
+    const container = createElement({
+        tagName: 'div',
+        className: ''
+    });
+    const img = createFighterImage(fighter);
+    const name = createElement({
+        tagName: 'p',
+        className: ''
+    });
+    name.innerText = fighter.name;
+    container.append(img, name);
     showModal({
         title: `Winner is ${fighter.name}`,
-        bodyElement: createFighterImage(fighter)
+        bodyElement: container
     });
 }
